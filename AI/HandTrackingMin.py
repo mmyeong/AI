f7865102461c0a487a835803cac00840c871f0c0
mmyeong <sso.mmyeong@gmail.com>
2021-04-04 15:16:29 +0900

    핸드포즈

--- AI/HandTrackingMin.py
+++ AI/HandTrackingMin.py
@@ -19,6 +19,12 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h,w,c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+
+                #if id==4:
+                cv2.circle(img,(cx,cy),15,(255,0,255),cv2.FILLED)
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
 
 
